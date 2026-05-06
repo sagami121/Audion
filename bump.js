fd666ffa22f5f659ae1dad9c3636709392338d84
sagami121 <sagami886@gmail.com>
2026-05-06 12:18:35 +0900

    prettierを追加

--- bump.js
+++ bump.js
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import { execFileSync } from 'child_process';
 
 const newVersion = process.argv[2];
 if (!newVersion) {
@@ -7,14 +8,21 @@ if (!newVersion) {
   process.exit(1);
 }
 
-//package.json
+function formatVersionFiles() {
+  const prettierBin = process.platform === 'win32' ? 'npx.cmd' : 'npx';
+  execFileSync(prettierBin, ['prettier', '--write', 'package.json', 'src-tauri/tauri.conf.json'], {
+    stdio: 'inherit'
+  });
+}
+
+// package.json
 const pkgPath = './package.json';
 const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'));
 pkg.version = newVersion;
 fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + '\n');
 console.log('Updated package.json');
 
-//package-lock.json
+// package-lock.json
 const lockPath = './package-lock.json';
 if (fs.existsSync(lockPath)) {
   const lock = JSON.parse(fs.readFileSync(lockPath, 'utf8'));
@@ -26,18 +34,20 @@ if (fs.existsSync(lockPath)) {
   console.log('Updated package-lock.json');
 }
 
-//tauri.conf.json
+// tauri.conf.json
 const tauriConfPath = './src-tauri/tauri.conf.json';
 const tauriConf = JSON.parse(fs.readFileSync(tauriConfPath, 'utf8'));
 tauriConf.version = newVersion;
 fs.writeFileSync(tauriConfPath, JSON.stringify(tauriConf, null, 2) + '\n');
 console.log('Updated src-tauri/tauri.conf.json');
 
-//Cargo.toml
+// Cargo.toml
 const cargoPath = './src-tauri/Cargo.toml';
 let cargo = fs.readFileSync(cargoPath, 'utf8');
 cargo = cargo.replace(/version = "(.*?)"/, `version = "${newVersion}"`);
 fs.writeFileSync(cargoPath, cargo);
 console.log('Updated src-tauri/Cargo.toml');
+
+formatVersionFiles();
 
 console.log(`\nSuccessfully bumped version to \x1b[32m${newVersion}\x1b[0m!`);
